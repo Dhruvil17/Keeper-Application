df1a540212300a7c9f74167d77dc78fdd00cb391
dhruvil <dhruvilsoni1703@gmail.com>
2022-03-19 18:25:10 +0530

    sample-commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<h1>hello world</h1>, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <App />
+    </div>,
+    document.getElementById('root')
+);
